3d1ecc21abc5f66a44c375c527c5a134bfb3e7ad
sajedmohit <sajedmht1379@gmail.com>
2023-11-24 14:16:02 +0200

    change message function

--- email-form.js
+++ email-form.js
@@ -1,16 +1,21 @@
 function sendEmail() {
   Email.send({
-    Host: "smtp.gmail.com",
+    Host: "smtp.elasticemail.com",
     Username: "sajedmht1379@gmail.com",
     Password: "FA6C3ADDD36001147437DBD331B432B629E3",
     To: "sajedmht1379@gmail.com",
     From: document.getElementById("email").value,
     Subject: "New contact form",
-    Body: "Name: " + document.getElementById("name").value
-          + "<br> Email: " + document.getElementById("email").value
-          + "<br> phone no: " + document.getElementById("phone").value
-          + "<br> message: " + document.getElementById("message").value
-  }).then((message) => alert("message sent succesfully :)"));
+    Body:
+      "Name: " +
+      document.getElementById("name").value +
+      "<br> Email: " +
+      document.getElementById("email").value +
+      "<br> phone no: " +
+      document.getElementById("phone").value +
+      "<br> message: " +
+      document.getElementById("message").value,
+  }).then(function () {
+    return alert("message sent succesfully :)");
+  });
 }
-// FA6C3ADDD36001147437DBD331B432B629E3
-// smtp.elasticemail.com
